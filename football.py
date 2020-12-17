7552e308fe5e1d87ef45b6126a64c55dcba7c85c
GameCap <59932236+GameCap@users.noreply.github.com>
2020-12-17 18:09:19 +0200

    football

--- football.py
+++ football.py
@@ -3,18 +3,43 @@ from random import randint
 
 W = 600
 H = 800
-j = -1
+#j = -1
 x, y = 0, 0
 STEP = 10
 FPS = 20
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
-g = randint(-30, 30)
-h = randint(-30, 30)
+g = 10
+h = 10
+
+score1 = 0
+score2 = 0
 
 pygame.init()
 surface = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Football")
+
+font = pygame.font.SysFont('verdana', 32)
+text1 = font.render(str(score1), True, BLUE) 
+textRect1 = text1.get_rect()  
+textRect1.center = (50, 50) 
+text2 = font.render(str(score2), True, BLUE) 
+textRect2 = text2.get_rect()  
+textRect2.center = (W - 50, 50) 
+
+class Gate1(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("gif/images.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+class Gate2(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("gif/images.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
 
 class Player1 (pygame.sprite.Sprite):
     def __init__(self, x, y, filename):
@@ -56,16 +81,24 @@ class Ball(pygame.sprite.Sprite):
         #self.image.set_colorkey(BLUE)
         self.rect = self.image.get_rect()
         self.rect.center = (300, 400)
+        self.dx = 1
+        self.dy = 1
     
     #Тут он начинает лететь в рандомную сторону но после тго как доходит до границы по х 
     #он начинает опускать вниз доходит до корая у и всё. Подскажите что делать пожалуйста.
     def update(self):
-        self.rect.x += g
-        self.rect.y += h
-        if self.rect.x > 550 or  self.rect.x < 50:
-            self.rect.x += j * g
-        if self.rect.y > 750 or self.rect.y < 50:
-            self.rect.y += j * h
+        self.rect.x += g * self.dx
+        self.rect.y += h * self.dy
+        if self.rect.x > 550 or self.rect.x < 0:
+            self.dx = -self.dx
+        if self.rect.y > 750 or self.rect.y < 0 :
+             self.dy = -self.dy
+        
+            
+                  
+            
+        #if self.rect.y > 700 or self.rect.y < 75:
+            #self.rect.y += j * h
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, x, y, filename):
@@ -75,11 +108,14 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (300, 400)
 
-
-pl1 = Player1(W//2, H//2, "images/z.png")
-pl2 = Player2(W//2, H//2, "images/z.png")
-ball = Ball(W//2, H//2, "images/ft.png") 
-bg = Background(W//2, H//2, "images/field.jpg")
+gate1 = pygame.sprite.Group() 
+gate1.add(Gate1(250, 0))
+gate2 = pygame.sprite.Group() 
+gate2.add(Gate2(250, 750))
+pl1 = Player1(W//2, H//2, "gif/z.png")
+pl2 = Player2(W//2, H//2, "gif/z.png")
+ball = Ball(W//2, H//2, "gif/ft.png") 
+bg = Background(W//2, H//2, "gif/field.jpg")
 clock = pygame.time.Clock()
 if __name__ == '__main__':
     counter = 0
@@ -101,10 +137,26 @@ while True:
         pl1.moveRight()
     elif keys[pygame.K_1]:
         pl1.moveLeft()
- 
+    
+    if pygame.sprite.spritecollideany(ball, gate1):
+            score1 += 1
+    if pygame.sprite.spritecollideany(ball, gate2):
+            score2 += 1
+
+    for i in gate1:
+            surface.blit(i.image, i.rect)
+    for i in gate2:
+            surface.blit(i.image, i.rect)
+     
     surface.fill(BLACK)
     surface.blit(bg.image, bg.rect)
     bg.update()
+    gate1.update()
+    gate2.update()
+    text1 = font.render(str(score1//2), True, BLUE)
+    surface.blit(text1, textRect1)
+    text2 = font.render(str(score2//2), True, BLUE)
+    surface.blit(text2, textRect2)
     surface.blit(pl1.image, pl1.rect)
     pl1.update()        
     surface.blit(pl2.image, pl2.rect)        
