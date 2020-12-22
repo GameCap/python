9f36b91fbe0d88a1dfc6b3b1b572d698e7488ecc
GameCap <59932236+GameCap@users.noreply.github.com>
2020-12-22 16:54:33 +0200

    football

--- football.py
+++ football.py
@@ -1,9 +1,9 @@
 import pygame
-from random import randint
+from random import randrange
 
 W = 600
 H = 800
-#j = -1
+j = randrange(-1, 1, 2)
 x, y = 0, 0
 STEP = 10
 FPS = 20
@@ -28,17 +28,19 @@ textRect2 = text2.get_rect()
 textRect2.center = (W - 50, 50) 
 
 class Gate1(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, filename):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("gif/images.jpg")
+        self.image = pygame.image.load(filename)
+        self.image.set_colorkey(BLUE)
         self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect.center = (250, 0)
 class Gate2(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, filename):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("gif/images.jpg")
+        self.image = pygame.image.load(filename)
+        self.image.set_colorkey(BLUE)
         self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect.center = (250, 750)
 
 
 class Player1 (pygame.sprite.Sprite):
@@ -93,8 +95,51 @@ class Ball(pygame.sprite.Sprite):
             self.dx = -self.dx
         if self.rect.y > 740 or self.rect.y < 10 :
              self.dy = -self.dy
-        
-            
+    
+    def handle_ball_collisions(self):
+        def intersect(obj, ball):
+            edges = dict(
+                left=rect(obj.left, obj.top, 1, obj.height),
+                right=rect(obj.right, obj.top, 1, obj.height),
+                top=rect(obj.left, obj.top, obj.width, 1),
+                bottom=rect(obj.left, obj.bottom, obj.width, 1))
+            collisions = set(edge for edge, rect in edges.items() if
+                            ball.bounds.colliderect(rect))
+            if not collisions:
+                return None
+    
+            if len(collisions) == 1:
+                return list(collisions)[0]
+    
+            if 'top' in collisions:
+                if ball.centery >= obj.top:
+                    return 'top'
+                if ball.centerx < obj.left:
+                    return 'left'
+                else:
+                    return 'right'
+    
+            if 'bottom' in collisions:
+                if ball.centery >= obj.bottom:
+                    return 'bottom'
+                if ball.centerx < obj.left:
+                    return 'left'
+                else:
+                    return 'right'
+        s = self.ball.speed
+        edge = intersect(self.paddle, self.ball)
+        if edge is not None:
+            self.sound_effects['paddle_hit'].play()
+        if edge == 'top':
+            g = s[0]
+            h = -s[1]
+            if self.paddle.moving_left:
+                g -= 1
+            elif self.paddle.moving_left:
+                g += 1
+            self.ball.speed = g, h
+        elif edge in ('left', 'right'):
+            self.ball.speed = (-s[0], s[1])
                   
             
         #if self.rect.y > 700 or self.rect.y < 75:
@@ -108,10 +153,8 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (300, 400)
 
-gate1 = pygame.sprite.Group() 
-gate1.add(Gate1(250, 0))
-gate2 = pygame.sprite.Group() 
-gate2.add(Gate2(250, 750))
+gate1 = Gate1(W//2, H//2, "gif/images.jpg")
+gate2 = Gate2(W//2, H//2, "gif/images.jpg")
 pl1 = Player1(W//2, H//2, "gif/z.png")
 pl2 = Player2(W//2, H//2, "gif/z.png")
 ball = Ball(W//2, H//2, "gif/ft.png") 
@@ -139,29 +182,33 @@ while True:
         pl1.moveLeft()
 
     
-    if pygame.sprite.spritecollideany(ball, gate1):
+    if pygame.sprite.collide_rect(ball, gate1):
             score1 += 1
             ball.rect.center = (300, 400)            
-    if pygame.sprite.spritecollideany(ball, gate2):
+    if pygame.sprite.collide_rect(ball, gate2):
             score2 += 1
             ball.rect.center = (300, 400)
 
-    if pygame.sprite.spritecollideany(ball, pl1):
-           ball.dx = -ball.dx
+    if pygame.sprite.collide_rect(ball, pl1):
+           ball.dx = -ball.dx * j
+           ball.dy = -ball.dy 
+    if pygame.sprite.collide_rect(ball, pl2):
+           ball.dx = -ball.dx * j
+           ball.dy = -ball.dy 
 
-    for i in gate1:
-            surface.blit(i.image, i.rect)
-    for i in gate2:
-            surface.blit(i.image, i.rect)
+    #for i in gate1:
+            #urface.blit(i.image,* i.rect)
+    #for i in gate2:
+           # surface.blit(i.image, i.rect)
      
     surface.fill(BLACK)
     surface.blit(bg.image, bg.rect)
     bg.update()
     gate1.update()
     gate2.update()
-    text1 = font.render(str(score1//2), True, BLUE)
+    text1 = font.render(str(score1), True, BLUE)
     surface.blit(text1, textRect1)
-    text2 = font.render(str(score2//2), True, BLUE)
+    text2 = font.render(str(score2), True, BLUE)
     surface.blit(text2, textRect2)
     surface.blit(pl1.image, pl1.rect)
     pl1.update()        
