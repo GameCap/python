1fe667e2f32cd7fc3db81e01ea0ffc9c6f21f3e6
GameCap <59932236+GameCap@users.noreply.github.com>
2020-12-22 20:40:56 +0200

    football

--- football.py
+++ football.py
@@ -3,7 +3,7 @@ from random import randrange
 
 W = 600
 H = 800
-j = randrange(-1, 1, 2)
+#j = randrange(-1, 1, 2)
 x, y = 0, 0
 STEP = 10
 FPS = 20
@@ -22,10 +22,10 @@ pygame.display.set_caption("Football")
 font = pygame.font.SysFont('verdana', 32)
 text1 = font.render(str(score1), True, BLUE) 
 textRect1 = text1.get_rect()  
-textRect1.center = (50, 50) 
+textRect1.center = (W - 50, 50) 
 text2 = font.render(str(score2), True, BLUE) 
 textRect2 = text2.get_rect()  
-textRect2.center = (W - 50, 50) 
+textRect2.center = (50, 50) 
 
 class Gate1(pygame.sprite.Sprite):
     def __init__(self, x, y, filename):
@@ -86,8 +86,6 @@ class Ball(pygame.sprite.Sprite):
         self.dx = 1
         self.dy = 1
     
-    #Тут он начинает лететь в рандомную сторону но после тго как доходит до границы по х 
-    #он начинает опускать вниз доходит до корая у и всё. Подскажите что делать пожалуйста.
     def update(self):
         self.rect.x += g * self.dx
         self.rect.y += h * self.dy
@@ -95,55 +93,6 @@ class Ball(pygame.sprite.Sprite):
             self.dx = -self.dx
         if self.rect.y > 740 or self.rect.y < 10 :
              self.dy = -self.dy
-    
-    def handle_ball_collisions(self):
-        def intersect(obj, ball):
-            edges = dict(
-                left=rect(obj.left, obj.top, 1, obj.height),
-                right=rect(obj.right, obj.top, 1, obj.height),
-                top=rect(obj.left, obj.top, obj.width, 1),
-                bottom=rect(obj.left, obj.bottom, obj.width, 1))
-            collisions = set(edge for edge, rect in edges.items() if
-                            ball.bounds.colliderect(rect))
-            if not collisions:
-                return None
-    
-            if len(collisions) == 1:
-                return list(collisions)[0]
-    
-            if 'top' in collisions:
-                if ball.centery >= obj.top:
-                    return 'top'
-                if ball.centerx < obj.left:
-                    return 'left'
-                else:
-                    return 'right'
-    
-            if 'bottom' in collisions:
-                if ball.centery >= obj.bottom:
-                    return 'bottom'
-                if ball.centerx < obj.left:
-                    return 'left'
-                else:
-                    return 'right'
-        s = self.ball.speed
-        edge = intersect(self.paddle, self.ball)
-        if edge is not None:
-            self.sound_effects['paddle_hit'].play()
-        if edge == 'top':
-            g = s[0]
-            h = -s[1]
-            if self.paddle.moving_left:
-                g -= 1
-            elif self.paddle.moving_left:
-                g += 1
-            self.ball.speed = g, h
-        elif edge in ('left', 'right'):
-            self.ball.speed = (-s[0], s[1])
-                  
-            
-        #if self.rect.y > 700 or self.rect.y < 75:
-            #self.rect.y += j * h
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, x, y, filename):
@@ -160,10 +109,10 @@ pl2 = Player2(W//2, H//2, "gif/z.png")
 ball = Ball(W//2, H//2, "gif/ft.png") 
 bg = Background(W//2, H//2, "gif/field.jpg")
 clock = pygame.time.Clock()
+
 if __name__ == '__main__':
     counter = 0
 while True:
-    counter += 1
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             pygame.quit()
@@ -190,16 +139,11 @@ while True:
             ball.rect.center = (300, 400)
 
     if pygame.sprite.collide_rect(ball, pl1):
-           ball.dx = -ball.dx * j
+           ball.dx = -ball.dx 
            ball.dy = -ball.dy 
     if pygame.sprite.collide_rect(ball, pl2):
-           ball.dx = -ball.dx * j
+           ball.dx = -ball.dx 
            ball.dy = -ball.dy 
-
-    #for i in gate1:
-            #urface.blit(i.image,* i.rect)
-    #for i in gate2:
-           # surface.blit(i.image, i.rect)
      
     surface.fill(BLACK)
     surface.blit(bg.image, bg.rect)
@@ -217,4 +161,22 @@ while True:
     surface.blit(ball.image, ball.rect)
     ball.update()    
     pygame.display.update()
-    pygame.time.delay(150)             
+    pygame.time.delay(50) 
+
+    if score1 == 1:
+        tx = font.render("Player 2 win",True,BLACK)
+        surface.blit(tx, [200,400])
+        ball.kill()
+        pl1.kill()
+        pl2.kill()
+        pygame.display.update()
+        pygame.time.delay(50)
+        
+    if score2 == 1:
+        tx = font.render("Player 1 win",True,BLACK)
+        surface.blit(tx, [200,400])
+        ball.kill()
+        pl1.kill()
+        pl2.kill()           
+        pygame.display.update()
+        pygame.time.delay(50)            
